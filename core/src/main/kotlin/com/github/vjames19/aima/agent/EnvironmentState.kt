e9eb87db64fe8d6c4fd210c9a2e514e2b52b4b27
Victor J. Reventos <victor.reventos1@gmail.com>
2017-08-08 15:48:05 -0500

    initial structure

---
+++ core/src/main/kotlin/com/github/vjames19/aima/agent/EnvironmentState.kt
@@ -0,0 +1,6 @@
+package com.github.vjames19.aima.agent
+
+/**
+ * Created by victor.reventos on 8/8/17.
+ */
+interface EnvironmentState
---
+++ core/src/main/kotlin/com/github/vjames19/aima/agent/Percept.kt
@@ -0,0 +1,7 @@
+package com.github.vjames19.aima.agent
+
+/**
+ * Created by victor.reventos on 8/8/17.
+ */
+interface Percept {
+}
